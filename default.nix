ead11d5a38f17fa6a7423394d31994bf5749427d
Julian Kahlert <90937526+juliankahlert@users.noreply.github.com>
2023-12-06 22:48:49 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,17 @@
-# default.nix
-
-{ lib, ... }:
-
 {
-  imports = [
-    <nixpkgs/nixos/modules/installer/iso-minimal.nix>
-  ];
-
-  boot.loader.grub.enable = true;
-  networking.hostName = "my-nixos-machine";
-  time.timeZone = "UTC";
+  description = "Minimal NixOS installation media";
+  inputs.nixos.url = "nixpkgs/23.11-beta";
+  outputs = { self, nixos }: {
+    nixosConfigurations = {
+      exampleIso = nixos.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          ({ pkgs, ... }: {
+            environment.systemPackages = [ pkgs.neovim ];
+          })
+        ];
+      };
+    };
+  };
 }
